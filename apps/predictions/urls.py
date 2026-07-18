9e7fd5ece6c8037ac513d24e2ce124947809798b
tatomesa <seba_mesa@hotmail.com>
2026-07-18 22:44:52 +0000

    Feat: agregando ranking general

--- apps/predictions/urls.py
+++ apps/predictions/urls.py
@@ -10,7 +10,7 @@ app_name = 'predictions'
 
 urlpatterns = [
     path('', UserPredictionsListView.as_view(), name='my_predictions'),
-    #path('ranking/', RankingView.as_view(), name='ranking'),
+    path('ranking/', RankingView.as_view(), name='ranking'),
     path('match/<int:match_pk>/create/', PredictionCreateUpdateView.as_view(), name='create'),
     path('<int:pk>/edit/', PredictionEditView.as_view(), name='edit'),
 ]
